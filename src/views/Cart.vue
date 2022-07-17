1efa2a8a31e6b09f663dfbe20acc6e18c9f0c180
Veys101 <vys2626@gmail.com>
2022-07-17 20:06:42 +0300

    cart added

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="cart">
-    <h1>This is an cart page</h1>
+    <h1>Your Cart</h1>
+    <CartItemCard v-for="product in products" :key="product.id" :product="product"/>
+    <CartSummaryPaymentCard />
   </div>
 </template>
+
+<script>
+  import CartItemCard from '../components/CartItemCard.vue'
+  import CartSummaryPaymentCard from '../components/CartSummaryPaymentCard.vue'
+
+  export default {
+    components: {
+      CartItemCard,
+      CartSummaryPaymentCard
+    },
+    computed: {
+      products() {
+        return this.$store.getters.cartItems
+      }
+    }
+  }
+
+</script>
