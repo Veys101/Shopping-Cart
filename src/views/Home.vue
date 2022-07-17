4a21ce6bfc924829291c4e0440a83a48e6c2089f
Veys101 <vys2626@gmail.com>
2022-07-17 20:07:26 +0300

    home and cart added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="home">
+    <ProductDescriptionDrawer
+      :product="product"
+      :active="active.product_drawer"
+      v-on:close-product-drawer="closeProductDrawer()" />
     
     <div class="product-cards-container">
       <ProductSummaryCard
@@ -15,22 +19,31 @@
 <script>
 import items from '../data/items.js'
 import ProductSummaryCard from '../components/ProductSummaryCard.vue'
+import ProductDescriptionDrawer from '../components/ProductDescriptionDrawer.vue'
 
 export default {
   name: 'Home',
   components: {
-    ProductSummaryCard
-  },
+    ProductSummaryCard,
+    ProductDescriptionDrawer
+},
   data() {
     return {
       items: items,
-      product: null
+      product: null,
+      active: {
+        product_drawer: false
+      }
     }
   },
   methods: {
     viewProduct(product) {
       this.product = product
+      this.active.product_drawer = true
       console.log(this.product)
+    },
+    closeProductDrawer() {
+      this.active.product_drawer = false
     }
   }
 }
